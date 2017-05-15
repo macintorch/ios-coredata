c4d51f09299ba06d635443adcb19bca0cf3a5b3f
Ainor Syahrizal <macintorch@gmail.com>
2017-05-15 14:51:39 +0800

    Initial Commit

---
+++ HitList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HitList
+//
+//  Created by Ainor Syahrizal on 15/05/2017.
+//  Copyright © 2017 Ainor Syahrizal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
