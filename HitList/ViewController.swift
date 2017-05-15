2db746624789d60f6ff3d1727d2b65374ef331db
Ainor Syahrizal <macintorch@gmail.com>
2017-05-15 15:15:46 +0800

    tableView setup

--- HitList/ViewController.swift
+++ HitList/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var names: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        title = "The List"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+
     }
 
+    @IBAction func addName(_ sender: UIBarButtonItem) {
+    }
+ 
 
+}
+
+// MARK: UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    cell.textLabel?.text = names[indexPath.row]
+        return cell
+    }
+    
 }
 
